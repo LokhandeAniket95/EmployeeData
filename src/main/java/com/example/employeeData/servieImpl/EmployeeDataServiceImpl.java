d064d75e4580ce6d1d6d5c633f26017efb8207fc
LokhandeAniket95 <39650484+LokhandeAniket95@users.noreply.github.com>
2022-02-20 15:18:42 +0530

    Update EmployeeDataServiceImpl.java
    message change

--- src/main/java/com/example/employeeData/servieImpl/EmployeeDataServiceImpl.java
+++ src/main/java/com/example/employeeData/servieImpl/EmployeeDataServiceImpl.java
@@ -52,7 +52,7 @@ public class EmployeeDataServiceImpl implements EmployeeDataService {
 				response = new ResponseEntity<>(employeeData.getEmail().toString(), headers, HttpStatus.OK);
 			}
 		} catch (HttpClientErrorException e) {
-			throw new EmployeeException("Records not found");
+			throw new EmployeeException("Records does not exist");
 		}
 		return response;
 	}
